a6c910857add570515586d74a322b9c93d6e0707
OmarAbuzgia <26505339@edgehill.ac.uk>
2025-10-07 14:48:42 +0100

    Feat: Complete Week 1 lab exercises

---
+++ week1_lab.py
@@ -0,0 +1,15 @@
+print("Hello, World!")
+
+# Storing text (a string)
+student_name = "Alex"
+# Storing a whole number (an integer)
+student_age = 20
+# Print the variables to the console
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+
+# A simple calculation
+num1 = 15
+num2 = 10
+total = num1 + num2
+print("The sum is:", total)
